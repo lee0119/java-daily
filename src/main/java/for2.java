094fed9f22056674a865451b936ebccd949ea68d
lee0119 <candogo91@gmail.com>
2022-09-21 21:09:37 +0900

    Study

--- src/main/java/for2.java
+++ src/main/java/for2.java
@@ -2,6 +2,7 @@ public class for2 {
     public static void main(String[] args) {
 
 
+        //구구단 안보고도 쓰기 
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
                 System.out.println(i + "*" + j + "=" + (i * j) + " ");
@@ -10,12 +11,4 @@ public class for2 {
         }
     }
 }
-
-for (int i = 1; i<10; i++){
-    for (int j=1; j <10; j++){
-         sout (i+"*"+j+"="+ (i*j)+ " ")
-        }
-    sout ""
-        }
-        {}
 
