215501ddbbdce47b176ca32c01325dd8991bef39
lee0119 <candogo91@gmail.com>
2022-09-23 12:36:30 +0900

    Study

--- src/main/java/for2.java
+++ src/main/java/for2.java
@@ -2,13 +2,13 @@ public class for2 {
     public static void main(String[] args) {
 
 
-        //구구단 안보고도 쓰기 
-        for (int i = 1; i < 10; i++) {
+        for (int i = 2; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
                 System.out.println(i + "*" + j + "=" + (i * j) + " ");
             }
             System.out.println("");
         }
     }
+
 }
 
