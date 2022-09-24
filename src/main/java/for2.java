91309b20282779b54111b010c31a83167998ffe4
lee0119 <candogo91@gmail.com>
2022-09-24 19:47:38 +0900

    Study

--- src/main/java/for2.java
+++ src/main/java/for2.java
@@ -1,7 +1,8 @@
 public class for2 {
     public static void main(String[] args) {
 
-
+        //다양한 방법으로 구구단
+        //오늘은 for문 
         for (int i = 2; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
                 System.out.println(i + "*" + j + "=" + (i * j) + " ");
