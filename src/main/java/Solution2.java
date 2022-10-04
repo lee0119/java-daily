a6bb466abc750e35461d1a321c26107897960464
lee0119 <candogo91@gmail.com>
2022-10-04 10:50:57 +0900

    Study

--- src/main/java/Solution2.java
+++ src/main/java/Solution2.java
@@ -6,24 +6,16 @@ import java.util.*;
 //시간없어서 복습 위주.
 
 //레벨  1 -14 소수찾기
+//레벨 1-15 평균 구하기
 class Solution2 {
-    public int solution(int n) {
-        int answer = 0;
-
-        for(int i = 2; i<=n;i++){
-            int temp = 0;
-            answer++;
-            for(int j=1;j<i;j++){
-                if(temp>1) {
-                    answer--;
-                    break;
-                }else if(i%j == 0) {
-                    temp++;
-                }
-            }
-
+    public double solution(int[] arr) {
+        double sum=0;
+        //for (int i=0;i<arr.length;i++){
+        //    sum += arr[i];
+        //}
+        for (int i : arr){
+            sum += i;
         }
-
-        return answer;
+        return sum/arr.length;
     }
 }
