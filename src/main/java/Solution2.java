996bf0bd917edee9d89ee309a8d0ed0874588e49
lee0119 <candogo91@gmail.com>
2022-10-24 00:37:00 +0900

    Study

--- src/main/java/Solution2.java
+++ src/main/java/Solution2.java
@@ -19,21 +19,18 @@ import java.util.*;
 //레벨 1-20 2106년
 //다시 풀기 2106
 //자바의 정석 예제
-//파이널 전이라 복습만 
+//파이널 전이라 복습만
+//백준 처음부터 
 import java.util.*;
-class Solution2 {
-    public String solution2(int a, int b) {
-        String answer[] = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
-        Calendar calendar = Calendar.getInstance();
-        // calendar 클래스의 month는 1월부터 12월을 0부터 11까지로 표현해야 됨
-        calendar.set(2016, a-1, b);
-        int res = calendar.get(Calendar.DAY_OF_WEEK);
-        return answer[res-1];
-    }
-}
-//다른 풀이
-class Solution {
-    public String solution(int a, int b) {
-        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
+public class Solution2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
     }
 }
