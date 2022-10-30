9dff4494234725f2306fd04e7fd79a51636cae36
lee0119 <candogo91@gmail.com>
2022-10-30 10:54:32 +0900

    Study

--- src/main/java/BaekJoon.java
+++ src/main/java/BaekJoon.java
@@ -2,42 +2,32 @@ import java.util.Scanner;
 
 public class BaekJoon {
 
-    public static void main(String[] args) {
+//백준 1011 - 풀이 방식 봤음  복습 다시
+        public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+            Scanner in = new Scanner(System.in);
 
-        int[][] dp = new int[30][30];
+            int T = in.nextInt();	// 테스트 케이스
 
-        // 2번 성질 (n == r, r == 0)
-        for (int i = 0; i < 30; i++) {
-            dp[i][i] = 1;
-            dp[i][0] = 1;
-        }
+            for(int i = 0; i < T; i++) {
 
+                int X = in.nextInt();
+                int Y = in.nextInt();
 
-        for (int i = 2; i < 30; i++) {
-            for (int j = 1; j < 30; j++) {
-                // 1번 성질
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                int distance = Y - X;	// 거리
+
+                int max = (int)Math.sqrt(distance);	// 소수점 버림
+
+                if(max == Math.sqrt(distance)) {
+                    System.out.println(max * 2 - 1);
+                }
+                else if(distance <= max * max + max) {
+                    System.out.println(max * 2);
+                }
+                else {
+                    System.out.println(max * 2 + 1);
+                }
+
             }
         }
-
-
-
-        int T = in.nextInt();
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < T; i++) {
-
-            // M개중 N개를 뽑는 경우이므로 nCr 에서 n = M, r = N이다.
-            int N = in.nextInt();	// N = r
-            int M = in.nextInt();	// M = n
-
-            sb.append(dp[M][N]).append('\n');
-        }
-
-        System.out.println(sb);
-
     }
-}
