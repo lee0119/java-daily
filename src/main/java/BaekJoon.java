b5ea6c9abafebf7e7f58cc270c49b1f86ec46722
lee0119 <candogo91@gmail.com>
2022-11-01 00:03:06 +0900

    Study

--- src/main/java/BaekJoon.java
+++ src/main/java/BaekJoon.java
@@ -1,33 +1,15 @@
-import java.util.Scanner;
 
+
+
+//기초 
 public class BaekJoon {
 
-//백준 1011 - 풀이 방식 봤음  복습 다시
-        public static void main(String[] args) {
-
-            Scanner in = new Scanner(System.in);
-
-            int T = in.nextInt();	// 테스트 케이스
-
-            for(int i = 0; i < T; i++) {
-
-                int X = in.nextInt();
-                int Y = in.nextInt();
-
-                int distance = Y - X;	// 거리
-
-                int max = (int)Math.sqrt(distance);	// 소수점 버림
-
-                if(max == Math.sqrt(distance)) {
-                    System.out.println(max * 2 - 1);
-                }
-                else if(distance <= max * max + max) {
-                    System.out.println(max * 2);
-                }
-                else {
-                    System.out.println(max * 2 + 1);
-                }
-
-            }
-        }
+    public static void main(String[] args) {
+//        int score = 99 + 1;
+        char number2 = 7 + '2';
+//        System.out.println(score);
+        System.out.println(number2);
+//        return number2;
     }
+}
+
