d26c7b967989312819d081bba3de966c2bc619ae
lee0119 <candogo91@gmail.com>
2022-11-03 00:43:21 +0900

    Study

--- src/main/java/BaekJoon.java
+++ src/main/java/BaekJoon.java
@@ -4,18 +4,22 @@
 //makeMarginalString
 //문자열을 입력받아 해당 문자열을 처음부터 한 글자(letter)씩 다시 작성하려고 합니다.
 //이 때, 한 글자를 추가할 때마다 부분적으로 완성된 문자열을 전부 이어붙인 문자열을 리턴해야 합니다.
+//fibonacci
+
 public class BaekJoon {
 
     public static void main(String[] args) {
-        public String makeMarginalString (String str){
+        public int[] fibonacci(int num) {
+            // TODO:
 
-            String result = "";
+            int[] fibo = new int[num + 1 ];
 
-             for (int i = 0; i < str.length(); i++) {
-             for (int j = 0; j <= i; j++) {
-                    result = result + str.charAt(j);
-                }
+                for (int i=0; i<=num; i++){
+                if (i<= 1) fibo[i] = i;
+                else fibo[i] =fibo[i - 2] + fibo[i -1 ];
             }
+            return fibo;
         }
+    }
 }
 
